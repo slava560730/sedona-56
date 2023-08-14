1c810582a8673a565fd1cc87cf0b798f8821dd19
koddaks <koss199710@gmail.com>
2023-08-14 16:53:17 +0300

    add script to close modal with event delegation

---
+++ scripts/closeModal.js
@@ -0,0 +1,20 @@
+let closeButton = document.querySelector(".modal-close-button");
+let modalContainer = document.querySelector(".modal-container");
+let modalButton = document.querySelector(".navigation-list__button");
+
+modalContainer.addEventListener("click", function (event) {
+  let target = event.target;
+
+  if (
+    target.className === "modal-close-button" ||
+    target.className === "modal-container"
+  ) {
+    console.log(target.className);
+    modalContainer.classList.add("visually-hidden");
+  }
+
+});
+
+modalButton.addEventListener("click", function () {
+  modalContainer.classList.remove("visually-hidden");
+});
